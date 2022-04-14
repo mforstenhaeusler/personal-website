f7cd6f05b5b59220be889a40e2dfd8a8d19baab3
mforstenhaeusler <Maxi@Maximilians-MacBook-Pro-5072.local>
2022-04-15 00:05:55 +0200

    update integrate.yml

--- src/next.config.js
+++ src/next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+/*const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = nextConfig*/
+
+const debug = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  reactStrictMode: true,
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+      /*"/about": { page: "/about" },*/
+    }
+  },
+  images: {
+    loader: "custom",
+  }
+  //assetPrefix: '',
+}
