f378c299b46426c3a76aedd7e615ef0f24471786
mforstenhaeusler <Maxi@Maximilians-MacBook-Pro-5072.local>
2022-04-15 01:53:49 +0200

    adds new next.js app

---
+++ src/next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
+
+const debug = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  reactStrictMode: true,
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+      /*"/about": { page: "/about" },*/
+    }
+  },
+  images: {
+    loader: "custom",
+  }
+  //assetPrefix: '',
+}
