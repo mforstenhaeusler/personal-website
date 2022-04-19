c08b689d575ca6dd68968b16a6215a3d128dfce1
mforstenhaeusler <Maxi@Maximilians-MacBook-Pro-5072.local>
2022-04-19 17:52:03 +0200

    adds link of cv manually

--- src/components/cv.js
+++ src/components/cv.js
@@ -3,9 +3,10 @@ import styles from '../styles/CV.module.css';
 
 export default function CV(){
     const cvLink = process.env.NEXT_PUBLIC_CV_LINK;
+    console.log(cvLink);
     return(<div className={styles.CVItem}>
         <p align="center">
-        <iframe src={cvLink} className={styles.CV}></iframe>
+        <iframe src="https://docs.google.com/document/d/e/2PACX-1vRIkVDOmOmWDrPTttWl4sNLSTFjxBI2_iYXjCsNl4wgf_-Y0tp-1VLh9zruJ9l8kMfM8uP1k5wyvWLQ/pub?embedded=true" className={styles.CV}></iframe>
         </p>
     </div>)
 }
