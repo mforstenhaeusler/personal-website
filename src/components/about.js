51ed881736b80a8722e78ed46ca082e940b7a75d
mforstenhaeusler <Maxi@Maximilians-MacBook-Pro-5072.local>
2022-04-20 10:59:13 +0200

    updates cv link

--- src/components/about.js
+++ src/components/about.js
@@ -8,7 +8,7 @@ const data = {
   "paragraph_highlighted": "",
   "paragraph": "I am currently pursing my graduate studies at the Technical Univeristy of Munich (TUM). Along side my studies, I work as a working \
   student at BMW, focussing on digitalizing of engineering research, development and production processes through the application of artificial \
-  intelligence. Further, I am an active member of a student organization centered around artifically intelligence, where I lead the \
+  intelligence. Further, I am an active member of a student organization centred around artifically intelligence, where I lead the \
   software development of web and cloud based applications. In general, I am a very open-minded person and eager to learn and \
   expand my knowledge about artifical intelligence, data anlytics, robotics and its applications.",
   "button": "Find out more about me!",
